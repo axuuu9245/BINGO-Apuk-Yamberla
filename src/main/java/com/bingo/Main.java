5e493ffe8bcb597e3722149b89458623b49c8f20
Apuk yamberla <ayyamberla@uce.edu.ec>
2026-06-03 13:16:02 -0500

    bingo

--- src/main/java/com/bingo/Main.java
+++ src/main/java/com/bingo/Main.java
@@ -7,23 +7,55 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-         int [][] bingo = new int[5][8];
-            for(int i =0;i<bingo.length;i++){
-                for(int j=0;j<bingo[i].length;j++){
-                    bingo[i][j]=(int)(Math.random()*100);
+         String [][] bingo = new String[5][5];
+            for(int i = 0; i < bingo.length; i++){
+                for(int j = 0; j < bingo[i].length; j++){
+                    int aux;
+                    boolean repetido;
+                do{
+                    repetido = false;
+                    aux = (int)(Math.random() * 100);
+
+            for(int f = 0; f < bingo.length; f++){
+                for(int c = 0; c < bingo[f].length; c++){
+                    if(bingo[f][c] != null &&
+                       Integer.parseInt(bingo[f][c]) == aux){
+                        repetido = true;
+                    }
                 }
             }
-
-            System.out.println("Bienvenidos al Juego de BINGO");
+        }while(repetido);
+        bingo[i][j] = String.valueOf(aux);
+    }
+}
             
             for(int i=0;i<bingo.length;i++){
-                for(int j=0;j<bingo.length;j++){
-                    System.out.print("  "+bingo[i][j]+" ");
+                for(int j=0;j<bingo[i].length;j++){
+                    System.out.print(bingo[i][j]+"\t");
                 }
                 System.out.println();
             }
 
-        
+            while(true){
+                System.out.println("Ingrese fila (0-4): ");
+                int fila = sc.nextInt();
+                System.out.println("Ingrese columna (0-4): ");
+                int columna = sc.nextInt();
+
+                if(fila >= 0 && fila < 5 && columna >= 0 && columna < 5){
+                bingo[fila][columna] = "x";
+                }else{
+                System.out.println("Coordenadas inválidas.");
+                }
+
+                for(int i=0;i<bingo.length;i++){
+                for(int j=0;j<bingo[i].length;j++){
+                    System.out.print(bingo[i][j]+"\t");
+                }
+                System.out.println();
+            }
+
+            }
 
 
     }
