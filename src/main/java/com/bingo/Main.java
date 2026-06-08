d2b89dace4a88ccae089f468ebf5a3f396e2d82c
Apuk yamberla <ayyamberla@uce.edu.ec>
2026-06-08 15:36:24 -0500

    juego bingo

--- src/main/java/com/bingo/Main.java
+++ src/main/java/com/bingo/Main.java
@@ -6,57 +6,50 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        String[][] bingo = new String[6][];
 
-         String [][] bingo = new String[5][5];
-            for(int i = 0; i < bingo.length; i++){
-                for(int j = 0; j < bingo[i].length; j++){
-                    int aux;
-                    boolean repetido;
-                do{
-                    repetido = false;
-                    aux = (int)(Math.random() * 100);
+        int min = 3;
+        int max = 5;
 
-            for(int f = 0; f < bingo.length; f++){
-                for(int c = 0; c < bingo[f].length; c++){
-                    if(bingo[f][c] != null &&
-                       Integer.parseInt(bingo[f][c]) == aux){
+        for (int i = 0; i < bingo.length; i++) {
+            int random = (int) (Math.random() * (max - min + 1)) + min;
+            bingo[i] = new String[random];
+            for (int j = 0; j < bingo[i].length; j++) {
+                int aux;
+                boolean repetido;
+                do {
+                 repetido = false;
+                 aux = (int) (Math.random() * 50) + 1;
+                    for (int f = 0; f < bingo.length; f++) {
+                        if (bingo[f] == null) {
+                            continue;
+                        }
+            for (int c = 0; c < bingo[f].length; c++) {
+                if (bingo[f][c] != null &&
+                     Integer.parseInt(bingo[f][c]) == aux) {
                         repetido = true;
+                        }
                     }
                 }
-            }
-        }while(repetido);
-        bingo[i][j] = String.valueOf(aux);
-    }
-}
-            
-            for(int i=0;i<bingo.length;i++){
-                for(int j=0;j<bingo[i].length;j++){
-                    System.out.print(bingo[i][j]+"\t");
+                } while (repetido);
+            bingo[i][j] = String.valueOf(aux);
+         }
+        }
+        System.out.print("Ingrese el número sorteado: ");
+        String numero = sc.next();
+        boolean encontrado = false;
+        for (int i = 0; i < bingo.length; i++) {
+            for (int j = 0; j < bingo[i].length; j++) {
+                if (bingo[i][j].equals(numero)) {
+                 bingo[i][j] = "X";
+                    encontrado = true;
                 }
-                System.out.println();
             }
-
-            while(true){
-                System.out.println("Ingrese fila (0-4): ");
-                int fila = sc.nextInt();
-                System.out.println("Ingrese columna (0-4): ");
-                int columna = sc.nextInt();
-
-                if(fila >= 0 && fila < 5 && columna >= 0 && columna < 5){
-                bingo[fila][columna] = "x";
-                }else{
-                System.out.println("Coordenadas inválidas.");
-                }
-
-                for(int i=0;i<bingo.length;i++){
-                for(int j=0;j<bingo[i].length;j++){
-                    System.out.print(bingo[i][j]+"\t");
-                }
-                System.out.println();
+        }
+            if (encontrado) {
+                System.out.println("Número encontrado.");
+            } else {
+             System.out.println("Número no encontrado.");
             }
-
-            }
-
-
     }
 }
